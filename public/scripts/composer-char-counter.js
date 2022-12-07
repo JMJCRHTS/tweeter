a2b46483ca21ab11435bf1fafdabd6efd169d865
Noor Chasib <noorchasib@gmail.com>
2022-12-06 20:17:55 -0800

    add character counting and clean up code

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  const form = $('#tweet-text');
+
+  form.on('input', function() {
+    let twtLength = $(this).val().length;
+    let counter = $(this).parent().find(".counter");
+    console.log(counter);
+
+    if (twtLength <= 140) {
+      $(counter).removeClass("red").html(140 - twtLength);
+    }
+    if (twtLength > 140) {
+      $(counter).addClass("red").html(140 - twtLength);
+    }
+
+  });
+});
