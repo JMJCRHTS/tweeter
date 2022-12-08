a7043da76210b4826ab08c06004dd0b11e5b8eaf
Noor Chasib <noorchasib@gmail.com>
2022-12-07 19:22:03 -0800

    implement tweets using ajax, add timeago and clean up small bits of code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,42 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {
 
+  const createTweetElement = function(tweetData) {
+
+    let time = timeago.format(tweetData.created_at);
+    
+    let $tweet = $('<article><header class="tweetheader"></header><p></p><footer></footer></article>');
+
+    $tweet.children('p').text(tweetData.content.text);
+
+    $tweet.children('.tweetheader').append(`<div><img src="${tweetData.user.avatars}" alt="avatar"><p>${tweetData.user.name}
+      </p></div><p>${tweetData.user.handle}</p>`);
+
+    $tweet.children('footer').append(`<p>${time}</p>
+      <div><i class="fa-solid fa-flag flag"></i><i class="fa-solid fa-retweet retweet"></i>
+      <i class="fa-solid fa-heart heart"></i></div>`);
+    
+    return $tweet;
+  };
+
+  const renderTweets = function(tweets) {
+    tweets.forEach(tweet => {
+      let $el = createTweetElement(tweet);
+      $('.tweetlist').append($el);
+    });
+  };
+
+  const loadTweets = function() {
+    $.get('/tweets').then((data) => renderTweets(data));
+  };
+
+  loadTweets();
+
+  $('.tweetform').submit(function(event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+
+    $.post('/tweets', data);
+
+  });
+
+});
