3f1c64e698bf38c55df38bb8e4d48e10eeaece2a
Noor Chasib <noorchasib@gmail.com>
2022-12-08 17:26:29 -0800

    implement desktop view, minor css changes and overall code clean

--- public/scripts/stretch-form.js
+++ public/scripts/stretch-form.js
@@ -18,9 +18,15 @@ $(document).ready(function() {
     $('html, body').animate({
       scrollTop: $('.tweetform').offset().top - 400
     }, 0);
-    newTwt.slideToggle();
+    if (window.scrollY < 250) {
+      newTwt.slideToggle();
+    }  else {
+      newTwt.slideDown();
+    }
     $('#tweet-text').trigger("focus");
   });
+
+
   scrollbtn.on("click", function() {
 
     $('html, body').animate({
@@ -33,13 +39,17 @@ $(document).ready(function() {
 
   $(window).scroll(function() {
     let scroll = $(window).scrollTop();
-    if (scroll >= 330) {
+    if (scroll >= 330 && window.innerWidth < 1024) {
       $('.logo').addClass("nav-el-bg");
       navbtn.fadeOut(200);
       scrollbtn.fadeIn(100);
     }
-    if (scroll < 330) {
+    if (scroll < 330 && window.innerWidth < 1024) {
       $('.logo').removeClass("nav-el-bg");
+      navbtn.fadeIn(200);
+      scrollbtn.fadeOut(200);
+    }
+    if (window.innerWidth > 1024) {
       navbtn.fadeIn(200);
       scrollbtn.fadeOut(200);
     }
